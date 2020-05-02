3a60813e0f982cdc139219ed06ce38f8c8c2a409
AurelianLorgar <dragon.priest.88@gmail.com>
2020-05-02 14:44:36 +0700

    Library updated / Библиотека улучшена

--- RomanToArabic.cs
+++ RomanToArabic.cs
@@ -2,6 +2,8 @@
 namespace RomanCalculator {
     public class RomanToArabic {
 
+        char[] numbers = { 'M', 'D', 'C', 'L', 'X', 'V', 'I' };
+
         public int Transfer(char[] arrayOfNumbers) {
 
             if (Check.CheckTransfer(arrayOfNumbers) == false) {
@@ -9,98 +11,90 @@ namespace RomanCalculator {
             }
 
             int arabicNumberInt = 0;
-            int checkPos = 0;
 
             for (int pos = 0; pos < arrayOfNumbers.Length; pos++) {
-                checkPos = pos + 1;
 
-                if (arrayOfNumbers[pos].Equals(null) || arrayOfNumbers.Equals(' ')) {
-                    arabicNumberInt += 0;
-                }
-
-                if (arrayOfNumbers[pos].Equals('M')) {
-                    arabicNumberInt += 1000;
-                }
-
-                if (arrayOfNumbers[pos].Equals('D')) {
-                    if (checkPos < arrayOfNumbers.Length) {
-                        if (arrayOfNumbers[pos + 1].Equals('M')) {
-                            arabicNumberInt -= 500;
-                        } else {
-                            arabicNumberInt += 500;
-                        }
-                    } else {
-                        arabicNumberInt += 500;
-                    }
-                }
-
-                if (arrayOfNumbers[pos].Equals('C')) {
-                    if (checkPos < arrayOfNumbers.Length) {
-                        if (arrayOfNumbers[pos + 1].Equals('M') || arrayOfNumbers[pos + 1].Equals('D')) {
-                            arabicNumberInt -= 100;
-                        } else {
-                            arabicNumberInt += 100;
-                        }
-                    } else {
-                        arabicNumberInt += 100;
-                    }
-                }
-
-                if (arrayOfNumbers[pos].Equals('L')) {
-                    if (checkPos < arrayOfNumbers.Length) {
-                        if (arrayOfNumbers[pos + 1].Equals('M') || arrayOfNumbers[pos + 1].Equals('D')
-                            || arrayOfNumbers[pos + 1].Equals('C')) {
-                            arabicNumberInt -= 50;
-                        } else {
-                            arabicNumberInt += 50;
-                        }
-                    } else {
-                        arabicNumberInt += 50;
-                    }
-                }
-
-                if (arrayOfNumbers[pos].Equals('X')) {
-                    if (checkPos < arrayOfNumbers.Length) {
-                        if (arrayOfNumbers[pos + 1].Equals('M') || arrayOfNumbers[pos + 1].Equals('D')
-                            || arrayOfNumbers[pos + 1].Equals('C') || arrayOfNumbers[pos + 1].Equals('L')) {
-                            arabicNumberInt -= 10;
-                        } else {
-                            arabicNumberInt += 10;
-                        }
-                    } else {
-                        arabicNumberInt += 10;
-                    }
-                }
-
-                if (arrayOfNumbers[pos].Equals('V')) {
-                    if (checkPos < arrayOfNumbers.Length) {
-                        if (arrayOfNumbers[pos + 1].Equals('M') || arrayOfNumbers[pos + 1].Equals('D')
-                            || arrayOfNumbers[pos + 1].Equals('C') || arrayOfNumbers[pos + 1].Equals('L')
-                            || arrayOfNumbers[pos + 1].Equals('X')) {
-                            arabicNumberInt -= 5;
-                        } else {
-                            arabicNumberInt += 5;
-                        }
-                    } else {
-                        arabicNumberInt += 5;
-                    }
-                }
-
-                if (arrayOfNumbers[pos].Equals('I')) {
-                    if (checkPos < arrayOfNumbers.Length) {
-                        if (arrayOfNumbers[pos + 1].Equals('M') || arrayOfNumbers[pos + 1].Equals('D')
-                            || arrayOfNumbers[pos + 1].Equals('C') || arrayOfNumbers[pos + 1].Equals('L')
-                            || arrayOfNumbers[pos + 1].Equals('X') || arrayOfNumbers[pos + 1].Equals('V')) {
-                            arabicNumberInt -= 1;
-                        } else {
-                            arabicNumberInt += 1;
-                        }
-                    } else {
-                        arabicNumberInt += 1;
-                    }
+                switch (arrayOfNumbers[pos]) {
+                    case 'M':
+                        arabicNumberInt += arabicNumbers(arrayOfNumbers, pos, 0);
+                        break;
+                    case 'D':
+                        arabicNumberInt += arabicNumbers(arrayOfNumbers, pos, 1);
+                        break;
+                    case 'C':
+                        arabicNumberInt += arabicNumbers(arrayOfNumbers, pos, 2);
+                        break;
+                    case 'L':
+                        arabicNumberInt += arabicNumbers(arrayOfNumbers, pos, 3);
+                        break;
+                    case 'X':
+                        arabicNumberInt += arabicNumbers(arrayOfNumbers, pos, 4);
+                        break;
+                    case 'V':
+                        arabicNumberInt += arabicNumbers(arrayOfNumbers, pos, 5);
+                        break;
+                    case 'I':
+                        arabicNumberInt += arabicNumbers(arrayOfNumbers, pos, 6);
+                        break;
+                    default:
+                        break;
                 }
             }
             return arabicNumberInt;
+        }
+
+        private int arabicNumbers(char[] arrayOfNumbers, int pos, int numberPos) {
+
+            if (numberPos == 0) {
+                return 1000;
+            }
+
+            switch (numberPos) {
+                case 1:
+                    if ((pos + 1) < arrayOfNumbers.Length) {
+                        if (arrayOfNumbers[pos].Equals(numbers[numberPos])) {
+                            return -500;
+                        }
+                    }
+                    return 500;
+                case 2:
+                    if ((pos + 1) < arrayOfNumbers.Length) {
+                        if (arrayOfNumbers[pos].Equals(numbers[numberPos])) {
+                            return -100;
+                        }
+                    }
+                    return 100;
+                case 3:
+                    if ((pos + 1) < arrayOfNumbers.Length) {
+                        if (arrayOfNumbers[pos].Equals(numbers[numberPos])) {
+                            return -50;
+                        }
+                    }
+                    return 50;
+                case 4:
+                    if ((pos + 1) < arrayOfNumbers.Length) {
+                        if (arrayOfNumbers[pos].Equals(numbers[numberPos])) {
+                            return -10;
+                        }
+                    }
+                    return 10;
+                case 5:
+                    if ((pos + 1) < arrayOfNumbers.Length) {
+                        if (arrayOfNumbers[pos].Equals(numbers[numberPos])) {
+                            return -5;
+                        }
+                    }
+                    return 5;
+                case 6:
+                    if ((pos + 1) < arrayOfNumbers.Length) {
+                        if (arrayOfNumbers[pos].Equals(numbers[numberPos])) {
+                            return -1;
+                        }
+                    }
+                    return 1;
+            }
+
+            return 0;
         }
     }
 }
